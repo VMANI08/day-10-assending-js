3b63110a26124a634685494f17c9f0c7d171b5c8
manikandan <vmmass08@gmail.com>
2022-06-11 09:59:26 +0530

    addfile.

---
+++ assending.js
@@ -0,0 +1,31 @@
+
+let a = 15, b=30, c=45;
+
+if(a<b)
+{
+    if(a<c)
+    {
+        if(b<c){
+            console.log(a,b,c)
+        }
+        else{
+            console.log(a,c,b)
+        }   
+    }
+    else
+    {
+        console.log(c,a,b)
+    }
+}
+else if(b<c){
+    if(a<c){
+        console.log(b,a,c)
+    }
+    else{
+        console.log(b,c,a)
+    }
+
+}
+else{
+    console.log(c,b,a)
+ }
